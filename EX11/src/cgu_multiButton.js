9647b175bd41a660684f1087f76b3aeb19844aff
b1329031 <b1329031@cgu.edu.tw>
2026-05-21 12:48:39 +0800

    upload

---
+++ EX11/src/cgu_multiButton.js
@@ -0,0 +1,26 @@
+import IconButton from '@mui/material/IconButton';
+import DeleteIcon from '@mui/icons-material/Delete';
+import AlarmIcon from '@mui/icons-material/Alarm';
+import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
+
+const MultiButton = (num) => {
+  var output = [];
+  output.push(
+    <IconButton key="cart" color="primary" aria-label="add to shopping cart">
+      <AddShoppingCartIcon />
+    </IconButton>
+  );
+  output.push(
+    <IconButton key="delete" color="primary" aria-label="delete">
+      <DeleteIcon />
+    </IconButton>
+  );
+  output.push(
+    <IconButton key="alarm" color="primary" aria-label="add an alarm">
+      <AlarmIcon />
+    </IconButton>
+  );
+  return output;
+};
+
+export default MultiButton;
